272392dc856005723978028b43896521b8edc637
RCNicolas <114864432+RCNicolas@users.noreply.github.com.>
2024-01-29 06:48:12 -0500

    feat: :tada: Creacion del repositorio con arquitectur MCV

---
+++ AldeaNinjaKonoha/src/main/java/com/mycompany/aldeaninjakonoha/habilidad/controller/HabilidadController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aldeaninjakonoha.habilidad.controller;
+
+/**
+ *
+ * @author nicos
+ */
+public class HabilidadController {
+    
+}
